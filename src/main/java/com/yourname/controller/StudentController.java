b845b32d14a8e191b60e325f7ae9c2f5d7bd1d5b
Tal Yaakov <tal.yaakov@sap.com>
2019-05-05 16:16:39 +0300

    Request

--- src/main/java/com/yourname/controller/StudentController.java
+++ src/main/java/com/yourname/controller/StudentController.java
@@ -2,13 +2,17 @@ package com.yourname.controller;
 
 import com.yourname.entity.Student;
 import com.yourname.service.StudentService;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Collection;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/students")
 public class StudentController {
 
@@ -18,6 +22,11 @@ public class StudentController {
     @RequestMapping(method = RequestMethod.GET)
     public Collection<Student> getAllStudents(){
         return studentService.getAllStudents();
+    }
+
+    @RequestMapping(value = "/tal", method = RequestMethod.GET)
+    public Response httpGetRequestSample() throws IOException {
+        return this.sampleHttpRequest();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -38,5 +47,25 @@ public class StudentController {
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void insertStudent(@RequestBody Student student){
         studentService.insertStudent(student);
+    }
+
+    private okhttp3.Response sampleHttpRequest() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        okhttp3.Request request = new okhttp3.Request.Builder()
+                .url("http://localhost:8080/students")
+                .get()
+//                .addHeader("User-Agent", "PostmanRuntime/7.11.0")
+                .addHeader("Accept", "*/*")
+//                .addHeader("Cache-Control", "no-cache")
+//                .addHeader("Postman-Token", "1074c41f-89d2-4db6-899f-91f2cf888ac6,bbad78e1-3088-49ca-9905-a697e0bc8943")
+                .addHeader("Host", "localhost:8080")
+//                .addHeader("accept-encoding", "gzip, deflate")
+//                .addHeader("Connection", "keep-alive")
+//                .addHeader("cache-control", "no-cache")
+                .build();
+
+        okhttp3.Response response = client.newCall(request).execute();
+        return response;
     }
 }
